31c9de2601714e4727e9de9e863747ea32cfc86c
lhw5123 <328066446@qq.com>
2015-11-09 18:23:45 +0800

    2015.11.09
    ProductofArrayExpectSelf

---
+++ src/com/hevin/array/ProductofArrayExpectSelf.java
@@ -0,0 +1,26 @@
+package com.hevin.array;
+
+/**
+ * Given an array of n integers where n > 1, nums,
+ * return an array output such that output[i] is equal to the product of
+ * all the elements of nums except nums[i].
+ * Solve it without division and in O(n).
+ * For example, given [1,2,3,4], return [24,12,8,6].
+ */
+public class ProductofArrayExpectSelf {
+    public int[] productExpectSelf(int[] nums) {
+        // 先求数左边每个数的乘积，再求右边每个数的乘积
+        int len = nums.length;
+        int[] res = new int[len];
+        res[0] = 1;
+        for (int i = 1; i < len; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
