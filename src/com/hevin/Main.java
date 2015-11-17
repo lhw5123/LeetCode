4cb36072152c2dab7a7990385144b761c695a70e
lhw5123 <328066446@qq.com>
2015-11-17 09:20:40 +0800

    2015.11.17
    调整项目结构

--- src/com/hevin/Main.java
+++ src/com/hevin/Main.java
@@ -1,13 +1,10 @@
 package com.hevin;
 
-import com.hevin.array.SearchForARange;
-import com.hevin.array.SearchInRotatedSortedArrayII;
-
-import java.util.Arrays;
+import com.hevin.array.RemoveDuplicatesII;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 2, 2, 3, 4};
-        System.out.println(Arrays.toString(SearchForARange.searchRange(nums, 2)));
+        int[] arr = new int[]{1, 1, 1, 2, 2, 2, 3};
+        System.out.println(new RemoveDuplicatesII().removeDuplicates(arr));
     }
 }
