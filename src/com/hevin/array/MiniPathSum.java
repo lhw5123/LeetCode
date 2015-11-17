615bc4f67013b2310240c554c3e147d7ee8e4d04
lhw5123 <328066446@qq.com>
2015-11-17 14:30:20 +0800

    MiniPathSum
    求矩阵从左上角到右下角的最小路径(动态规划)

---
+++ src/com/hevin/array/MiniPathSum.java
@@ -0,0 +1,26 @@
+package com.hevin.array;
+
+/**
+ * Given a m x n grid filled with non-negative numbers,
+ * find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * Note: You can only move either down or right at any point in time.
+ */
+public class MiniPathSum {
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int columns = grid[0].length;
+        // 用动态规划求解
+        for (int i = 1; i < columns; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+        for (int i = 1; i < rows; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                grid[i][j] += grid[i - 1][j] < grid[i][j - 1] ? grid[i - 1][j] : grid[i][j - 1];
+            }
+        }
+        return grid[rows - 1][columns - 1];
+    }
+}
