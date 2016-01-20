9670bd3817021a2481fb2c08ee4438b13c285460
Hevin <328066446@qq.com>
2016-01-20 23:09:09 +0800

    new questions.

---
+++ src/com/hevin/array/SubsetsII.java
@@ -0,0 +1,19 @@
+package com.hevin.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a collection of integers that might contain duplicates, nums,
+ * return all possible subsets.
+ * Note:
+ * Elements in a subset must be in non-descending order.
+ * The solution set must not contain duplicate subsets.
+ */
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        return res;
+    }
+}
