45fb43a4d3a6339bac59558e8d833fa5158304f4
Hevin <liuhw@jpush.cn>
2016-07-12 23:42:38 +0800

    Two sum.

---
+++ src/com/hevin/array/TwoSum.java
@@ -0,0 +1,23 @@
+package com.hevin.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution.
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solutions.");
+    }
+}
