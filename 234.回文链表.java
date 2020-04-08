1b8e8238074d08962f1900e7d2419c2dfaa9cfda
Hevin <hev1n@qq.com>
2020-04-08 17:54:58 +0800

    234.回文链表

---
+++ 234.回文链表.java
@@ -0,0 +1,56 @@
+import java.util.LinkedList;
+
+/*
+ * @lc app=leetcode.cn id=234 lang=java
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow = reversed(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reversed(ListNode head) {
+        ListNode prev = null, cur = head, next;
+
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+}
+// @lc code=end
+
