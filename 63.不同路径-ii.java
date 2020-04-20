3d86cbfd3435569efe90b6482ff902b3405aa1fb
Hevin <hev1n@qq.com>
2020-04-20 22:25:55 +0800

    63.不同路径-ii

---
+++ 63.不同路径-ii.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=java
+ *
+ * [63] 不同路径 II
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) {
+            return 0;
+        }
+
+        int rows = obstacleGrid.length;
+        int columns = obstacleGrid[0].length;
+        int[][] dp = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int j = 0; j < columns; j++) {
+            if (obstacleGrid[0][j] == 0) {
+                dp[0][j] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                if (obstacleGrid[i][j] != 1) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[rows - 1][columns - 1];
+    }
+}
+// @lc code=end
+
