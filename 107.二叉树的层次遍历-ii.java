437497f564ab95aaf1ad152117e741e91310a09b
hevin.liu <hevin.liu@iglooinsure.com>
2020-09-06 21:03:27 +0800

    107.二叉树的层次遍历-ii.java

---
+++ 107.二叉树的层次遍历-ii.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=107 lang=java
+ *
+ * [107] 二叉树的层次遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+
+        if (root == null) {
+            return list;
+        }
+
+        dfs(root, 0, list);
+        Collections.reverse(list);
+        return list;
+    }
+
+    public void dfs(TreeNode root, int level, List<List<Integer>> list) {
+        if (root == null) {
+            return;
+        }
+
+        if (list.size() < level + 1) {
+            list.add(new ArrayList<Integer>());
+        }
+
+        list.get(level).add(root.val);
+        dfs(root.left, level + 1, list);
+        dfs(root.right, level + 1, list);
+    }
+}
+// @lc code=end
+
