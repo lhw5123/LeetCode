3545ef58540dc4e13ca466d5399073eeed36ac27
hevin <hevin.liu@iglooinsure.com>
2021-01-18 21:50:29 +0800

    103.二叉树的锯齿形层序遍历.go

---
+++ 103.二叉树的锯齿形层序遍历.go
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=golang
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	var res [][]int
+	search(root, 0, &res)
+	return res
+}
+
+// 递归解法
+func search(root *TreeNode, depth int, res *[][]int) {
+	if root == nil {
+		return
+	}
+
+	for len(*res) < depth+1 {
+		*res = append(*res, []int{})
+	}
+
+	if depth%2 == 0 {
+		(*res)[depth] = append((*res)[depth], root.Val)
+	} else {
+		(*res)[depth] = append([]int{root.Val}, (*res)[depth]...)
+	}
+
+	search(root.Left, depth+1, res)
+	search(root.Right, depth+1, res)
+}
+
+// @lc code=end
+
