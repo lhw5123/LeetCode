d881ab1baadbed7ab6654868626c25f35742a730
hevin <hevin.liu@iglooinsure.com>
2021-04-26 17:13:54 +0800

    COMPLETE-417.太平洋大西洋水流问题

--- 417.太平洋大西洋水流问题.go
+++ 417.太平洋大西洋水流问题.go
@@ -5,67 +5,63 @@
  */
 
 // @lc code=start
+// 从四边出发做 bfs，最后对两个 visited 数据求交集，得到可以同时到达 Pacific 和 Atlantic 的位置。
 func pacificAtlantic(heights [][]int) [][]int {
 	rows := len(heights)
 	cols := len(heights[0])
 
+	var qp, qa []int
+	vp := make([]bool, rows*cols)
+	va := make([]bool, rows*cols)
+
+	for y := 0; y < rows; y++ {
+		qp = append(qp, getIndex(0, y, cols))      // left
+		qa = append(qa, getIndex(cols-1, y, cols)) // right
+	}
+
+	for x := 0; x < cols; x++ {
+		qp = append(qp, getIndex(x, 0, cols))      // top
+		qa = append(qa, getIndex(x, rows-1, cols)) // bottom
+	}
+
+	bfs(heights, &qp, &vp, rows, cols)
+	bfs(heights, &qa, &va, rows, cols)
+
 	res := [][]int{}
-	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
-			if bfs(heights, i, j, rows, cols) {
-				res = append(res, []int{i, j})
+	for y := 0; y < rows; y++ {
+		for x := 0; x < cols; x++ {
+			index := getIndex(x, y, cols)
+			if vp[index] && va[index] {
+				res = append(res, []int{y, x})
 			}
 		}
 	}
+
 	return res
 }
 
-func bfs(heights [][]int, i, j, rows, cols int) bool {
-	index := getIndex(i, j, cols)
-
-	var queue []int
-	queue = append(queue, index)
-
-	couldArriveAtlantic := false
-	couldArrivePacific := false
-
-	visited := make([]bool, rows*cols)
-	visited[index] = true
-
-	for len(queue) > 0 {
-		front := queue[0]
-		queue = queue[1:]
-		curX := front / cols
-		curY := front % cols
-
-		if !couldArrivePacific && arrivePacific(curX, curY, rows, cols) {
-			couldArrivePacific = true
-		}
-		if !couldArriveAtlantic && arriveAtlantic(curX, curY, rows, cols) {
-			couldArriveAtlantic = true
-		}
-		if couldArriveAtlantic && couldArrivePacific {
-			return true
+func bfs(heights [][]int, q *[]int, v *[]bool, rows, cols int) {
+	for len(*q) > 0 {
+		front := (*q)[0]
+		*q = (*q)[1:]
+		if (*v)[front] {
+			continue
 		}
 
-		for _, direction := range directions {
-			// if couldArrivePacific && i < 2 {
-			// 	continue
-			// }
-			// if couldArriveAtlantic && i >= 2 {
-			// 	continue
-			// }
-			newX := curX + direction[0]
-			newY := curY + direction[1]
+		x := front % cols
+		y := front / cols
+		h := heights[y][x]
+		(*v)[front] = true
+
+		for _, dir := range directions {
+			newX := x + dir[0]
+			newY := y + dir[1]
 			newIndex := getIndex(newX, newY, cols)
-			if inArea(newX, newY, rows, cols) && heights[newX][newY] <= heights[curX][curY] && !visited[newIndex] {
-				queue = append(queue, newIndex)
-				visited[newIndex] = true
+			if inArea(newX, newY, rows, cols) && heights[newY][newX] >= h && !(*v)[newIndex] {
+				*q = append(*q, newIndex)
 			}
 		}
 	}
-
-	return false
 }
 
 var directions = [][]int{
@@ -75,20 +71,12 @@ var directions = [][]int{
 	{0, 1},
 }
 
-func arrivePacific(i, j, rows, cols int) bool {
-	return i == 0 || j == 0
-}
-
-func arriveAtlantic(i, j, rows, cols int) bool {
-	return i == cols-1 || j == rows-1
-}
-
 func getIndex(x, y, cols int) int {
-	return x*cols + y
+	return y*cols + x
 }
 
 func inArea(x, y, rows, cols int) bool {
-	return x >= 0 && x < rows && y >= 0 && y < cols
+	return x >= 0 && x < cols && y >= 0 && y < rows
 }
 
 // @lc code=end
