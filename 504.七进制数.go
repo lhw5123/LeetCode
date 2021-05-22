da48e2291523f29517c086cfac79bf92c10f90cd
hevin <hev1n@qq.com>
2021-05-22 20:59:23 +0800

    504.七进制数.go

---
+++ 504.七进制数.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=504 lang=golang
+ *
+ * [504] 七进制数
+ */
+
+// @lc code=start
+func convertToBase7(num int) string {
+	if num == 0 {
+		return "0"
+	}
+
+	res := ""
+	tmp := num
+	negative := false
+	if tmp < 0 {
+		negative = true
+		tmp = -tmp
+	}
+
+	for tmp != 0 {
+		res = fmt.Sprintf("%d%s", tmp%7, res)
+		tmp /= 7
+	}
+	if negative {
+		res = "-" + res
+	}
+	return res
+}
+
+// @lc code=end
+
