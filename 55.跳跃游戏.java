b9f8647b240666a1fb51d83628837fde5ed5555d
hevin <hev1n@qq.com>
2021-07-06 17:28:10 +0800

    55.跳跃游戏.java

---
+++ 55.跳跃游戏.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=java
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightMost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightMost) {
+                rightMost = Math.max(rightMost, i + nums[i]);
+                if (rightMost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
