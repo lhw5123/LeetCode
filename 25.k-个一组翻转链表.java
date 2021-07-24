5cace83b4e76d6ac674764adaf256d4ddd05a0ed
hevin <hev1n@qq.com>
2021-07-24 14:05:44 +0800

    25.k-个一组翻转链表.java

---
+++ 25.k-个一组翻转链表.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=java
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // 递归
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode p = head;
+        for (int i = 0; i < k; i++) {
+            if (p == null) {
+                return head;
+            }
+            p = p.next;
+        }
+        ListNode newHead = reverse(head, p);
+        head.next = reverseKGroup(p, k);
+        return newHead;
+    }
+
+    public ListNode reverse(ListNode first, ListNode last) {
+        ListNode prev = null;
+        ListNode cur = first;
+        while (cur != last) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
+// @lc code=end
+
