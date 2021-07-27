c99fe4a8874c3401942ba851fd0e71447d05a466
hevin <hev1n@qq.com>
2021-07-27 15:50:20 +0800

    283.移动零.java

---
+++ 283.移动零.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=java
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int len = nums.length;
+        int left = 0, right = 0;
+        while (right < len) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+            right++;
+        }
+    }
+}
+// @lc code=end
+
