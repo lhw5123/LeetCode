24350562c6c249a7af89bd94a20d632a075de24f
hevin <hev1n@qq.com>
2021-08-17 21:02:25 +0800

    448.找到所有数组中消失的数字.java

---
+++ 448.找到所有数组中消失的数字.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=448 lang=java
+ *
+ * [448] 找到所有数组中消失的数字
+ */
+
+// @lc code=start
+class Solution {
+    // 关键是在值和下标之前的转换。
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;  // 求下标
+            nums[x] += n;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
