87d16e7e1566498de4f9b220e2925d71873b942c
hevin <hev1n@qq.com>
2021-08-21 15:00:42 +0800

    300.最长递增子序列.java

---
+++ 300.最长递增子序列.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int res = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
