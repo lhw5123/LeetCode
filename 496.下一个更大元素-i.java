5ee98f08a5eb84a2cb07c6b6f3883f3a0183a832
hevin <hev1n@qq.com>
2021-08-26 22:03:55 +0800

    496.下一个更大元素-i.java

---
+++ 496.下一个更大元素-i.java
@@ -0,0 +1,38 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=496 lang=java
+ *
+ * [496] 下一个更大元素 I
+ */
+
+// @lc code=start
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Deque<Integer> stack = new LinkedList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
