418db73eb2dc2705de625d9da5fe25672eb74ead
hevin <hev1n@qq.com>
2021-10-03 21:12:09 +0800

    92.翻转链表-ii.java

---
+++ 92.反转链表-ii.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=java
+ *
+ * [92] 反转链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode newHead = new ListNode(0, head);
+        ListNode pre = newHead;
+        for (int i = 0; pre.next != null && i < left - 1; i++) {
+            pre = pre.next;
+        }
+        if (pre.next == null) {
+            return head;
+        }
+
+        ListNode cur = pre.next;
+        for (int i = 0; i < right - left; i++) {
+            ListNode next = pre.next;
+            pre.next = cur.next;
+            cur.next = cur.next.next;
+            pre.next.next = next;
+        }
+        return newHead.next;
+    }
+}
+// @lc code=end
+
