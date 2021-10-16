11c958a89f5c7894aa922d2ccf29d74d1bf1b196
hevin <hev1n@qq.com>
2021-10-16 17:45:17 +0800

    82.删除排序链表中的重复元素-ii.java

---
+++ 82.删除排序链表中的重复元素-ii.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=82 lang=java
+ *
+ * [82] 删除排序链表中的重复元素 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummyHead = new ListNode(Integer.MIN_VALUE, head);
+        ListNode cur = dummyHead;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int val = cur.next.val;
+                while (cur.next != null && cur.next.val == val) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummyHead.next;
+    }
+}
+// @lc code=end
+
