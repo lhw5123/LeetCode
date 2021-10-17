23b9511c9e59589abb4456fe1cd14e3401017692
hevin <hev1n@qq.com>
2021-10-17 15:53:54 +0800

    322.零钱兑换.java

---
+++ 322.零钱兑换.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=java
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = amount + 1;
+        }
+
+        for (int amt = 1; amt <= amount; amt++) {
+            for (int c = 0; c < coins.length; c++) {
+                int coin = coins[c];
+                if (coin <= amt) {
+                    if (dp[amt] > dp[amt - coin] + 1) {
+                        dp[amt] = dp[amt - coin] + 1;
+                    }
+                }
+            }
+        }
+
+        if (dp[amount] == amount + 1) {
+            return -1;
+        }
+        return dp[amount];
+    }
+}
+// @lc code=end
+
