c547c4def2d2f7f99704f0eab735d3fac2a7f457
hevin <hev1n@qq.com>
2021-11-17 09:20:18 +0800

    0.快速排序.java

---
+++ 0.快速排序.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = nums[left];
+        int i = left;
+        int j = right;
+        while (i < j) {
+            while (i < j && nums[j] >= pivot) {
+                j--;
+            }
+            nums[i] = nums[j];
+            while (i < j && nums[i] <= pivot) {
+                i++;
+            }
+            nums[j] = nums[i];
+        }
+        nums[i] = pivot;
+        quickSort(nums, left, i - 1);
+        quickSort(nums, i + 1, right);
+    }
+}
