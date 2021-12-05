44e79c0197c9aa5dca963b25f421a7b2c1eec241
hevin <hev1n@qq.com>
2021-12-05 18:41:21 +0800

    152.乘积最大子数组.java

--- 152.乘积最大子数组.java
+++ 152.乘积最大子数组.java
@@ -7,13 +7,16 @@
 // @lc code=start
 class Solution {
     public int maxProduct(int[] nums) {
-        // 因为 nums 中同时存在正、负数，所以需要同时记录下最小值。
         int max = nums[0], min = nums[0], res = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            int mx = max, mn = min;
-            max = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
-            min = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
-            res = Math.max(max, res);
+            if (nums[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(nums[i], max * nums[i]);
+            min = Math.min(nums[i], min * nums[i]);
+            res = Math.max(res, max);
         }
         return res;
     }
