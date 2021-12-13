233b83dbc9fc1ad7d9fadd25c327909e7a7ab2a4
hevin <hev1n@qq.com>
2021-12-13 21:36:12 +0800

    75.颜色分类.java

--- 75.颜色分类.java
+++ 75.颜色分类.java
@@ -2,6 +2,10 @@
  * @lc app=leetcode.cn id=75 lang=java
  *
  * [75] 颜色分类
+ * 
+ * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，
+ * 使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
  */
 
 // @lc code=start
@@ -11,23 +15,24 @@ class Solution {
             return;
         }
 
-        int p = 0, q = nums.length - 1;
-        for (int i = 0; i <= q; i++) {
+        // left 和 right 其实代表的是红色、白色和蓝色的边界
+        int left = 0, right = nums.length - 1;
+        for (int i = 0; i <= right; i++) {
             if (nums[i] == 0) {
-                nums[i] = nums[p];
-                nums[p] = 0;
-                p++;
-            }
-            if (nums[i] == 2) {
-                nums[i] = nums[q];
-                nums[q] = 2;
-                q--;
-                if (nums[i] != 1) {
-                    i--;
-                }
+                swap(nums, i, left);
+                left++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, right);
+                right--;
+                i--;
             }
         }
     }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
 // @lc code=end
-
