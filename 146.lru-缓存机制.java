82ff9b71a5fa03f11a75c9ecf65cfa838999284a
hevin <hev1n@qq.com>
2022-01-27 18:54:34 +0800

    146.lru-缓存机制.java

--- 146.lru-缓存机制.java
+++ 146.lru-缓存机制.java
@@ -1,7 +1,4 @@
-import java.awt.List;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 /*
@@ -23,18 +20,42 @@ class Node {
     }
 }
 
-class LRUCache {
-    private int cap;
-    private Map<Integer, Node> map;
+// 避免命名冲突
+class MList {
     Node head, tail;
 
-    public LRUCache(int capacity) {
-        cap = capacity;
-        map = new HashMap<Integer, Node>();
+    MList() {
         head = new Node(0, 0);
         tail = new Node(0, 0);
         head.next = tail;
         tail.prev = head;
+    }
+
+    void add(Node node) {
+        Node next = head.next;
+        head.next = node;
+        node.next = next;
+        next.prev = node;
+        node.prev = head;
+    }
+
+    void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+}
+
+class LRUCache {
+    private int cap;
+    private Map<Integer, Node> map;
+    private MList list;
+
+    public LRUCache(int capacity) {
+        cap = capacity;
+        map = new HashMap<Integer, Node>();
+        list = new MList();
     }
     
     public int get(int key) {
@@ -42,40 +63,24 @@ class LRUCache {
         if (node == null) {
             return -1;
         }
-        remove(node);
-        add(node);
+        list.remove(node);
+        list.add(node);
         return node.val;
     }
     
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            remove(map.get(key));
+            list.remove(map.get(key));
         }
         Node node = new Node(key, value);
         map.put(key, node);
-        add(node);
+        list.add(node);
 
         if (map.size() > cap) {
-            Node last = tail.prev;
-            remove(last);
+            Node last = list.tail.prev;
+            list.remove(last);
             map.remove(last.key);
         }
-    }
-
-    // 头插
-    private void add(Node node) {
-        Node next = head.next;
-        next.prev = node;
-        node.prev = head;
-        node.next = next;
-        head.next = node;
-    }
-
-    private void remove(Node node) {
-        Node prev = node.prev;
-        Node next = node.next;
-        prev.next = next;
-        next.prev = prev;
     }
 }
 
