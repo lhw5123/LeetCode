e8945f198ef095466053575747d38ddaef32de1a
hevin <hev1n@qq.com>
2022-02-07 17:56:01 +0800

    283.移动0.java

--- 283.移动零.java
+++ 283.移动零.java
@@ -2,24 +2,28 @@
  * @lc app=leetcode.cn id=283 lang=java
  *
  * [283] 移动零
+ * 将数组中的所有 0 移动到尾端。
  */
 
 // @lc code=start
 class Solution {
     public void moveZeroes(int[] nums) {
         int len = nums.length;
-        // left: 最后一个不是 0 的位置
-        // right: 正常的遍历指针
-        int left = 0, right = 0;
-        while (right < len) {
-            if (nums[right] != 0) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                left++;
+        // l 指向最后一个为 0 的元素
+        int l = 0, r = 0;
+        while (r < len) {
+            if (nums[r] != 0) {
+                swap(nums, l, r);
+                l++;
             }
-            right++;
+            r++;
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
 // @lc code=end
