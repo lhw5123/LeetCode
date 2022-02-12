04eb6b0f7f10f8cee05f87ceb2f307ec38548037
hevin <hev1n@qq.com>
2022-02-12 19:12:12 +0800

    647.回文子串.java

--- 647.回文子串.java
+++ 647.回文子串.java
@@ -9,16 +9,23 @@ class Solution {
     public int countSubstrings(String s) {
         int res = 0;
         int len = s.length();
-        for (int i = 0; i < 2 * len - 1; i++) {
-            int l = i / 2;
-            int r = i / 2 + i % 2;
-            while (l >= 0 && r < len && s.charAt(l) == s.charAt(r)) {
-                res++;
-                l--;
-                r++;
-            }
+        for (int i = 0; i < len; i++) {
+            res += count(s, i, i);  // 回文子串长度是奇数
+            res += count(s, i, i + 1);  // 回文子串长度是偶数
         }
         return res;
+    }
+
+    private int count(String s, int start, int end) {
+        int len = s.length();
+        int count = 0;
+        while (start >= 0 && end < len && s.charAt(start) == s.charAt(end)) {
+            count++;
+            // 从中心扩展，因此是 start--，end++
+            start--;
+            end++;
+        }
+        return count;
     }
 }
 // @lc code=end
