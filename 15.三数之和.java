f95440c70a78f356162ac68b71e3c6115328db91
hevin <hev1n@qq.com>
2022-02-13 18:40:39 +0800

    15.三数之和.java

--- 15.三数之和.java
+++ 15.三数之和.java
@@ -28,20 +28,15 @@ class Solution {
     }
 
     // 要保证传入的 nums 必须是有序的。
+    // 返回的是两个值，而不是下标。
     private List<List<Integer>> twoSum(int[] nums, int start, int target) {
         List<List<Integer>> res = new ArrayList<>();
+
         int l = start, r = nums.length - 1;
+        // 在 while 中做索引移动，一定要重复判断索引关系。
         while (l < r) {
             int lv = nums[l], rv = nums[r];
-            if (lv + rv < target) {
-                while (l < r && nums[l] == lv) {
-                    l++;
-                }
-            } else if (lv + rv > target) {
-                while (l < r && nums[r] == rv) {
-                    r--;
-                }
-            } else if (lv + rv == target) {
+            if (lv + rv == target) {
                 res.add(Arrays.asList(lv, rv));
                 while (l < r && nums[l] == lv) {
                     l++;
@@ -49,8 +44,17 @@ class Solution {
                 while (l < r && nums[r] == rv) {
                     r--;
                 }
+            } else if (lv + rv < target) {
+                while (l < r && nums[l] == lv) {
+                    l++;
+                }
+            } else {
+                while (l < r && nums[r] == rv) {
+                    r--;
+                }
             }
         }
+
         return res;
     }
 }
