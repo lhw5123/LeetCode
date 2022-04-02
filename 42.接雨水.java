b67cee026413f3754e3ee21195af9b54ba9cbf59
hevin <hev1n@qq.com>
2022-04-02 23:31:24 +0800

    42.接雨水.java

--- 42.接雨水.java
+++ 42.接雨水.java
@@ -8,18 +8,18 @@
 class Solution {
     // two-pointers
     public int trap(int[] height) {
-        int res = 0;
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
+        int res = 0;
         while (left < right) {
             leftMax = Math.max(leftMax, height[left]);
             rightMax = Math.max(rightMax, height[right]);
-            if (rightMax > leftMax) {
+            if (leftMax < rightMax) {
                 res += leftMax - height[left];
-                ++left;
+                left++;
             } else {
                 res += rightMax - height[right];
-                --right;
+                right--;
             }
         }
         return res;
