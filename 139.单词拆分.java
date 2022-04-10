134fac291e6f824b4a060607efdf58249a72c253
hevin <hev1n@qq.com>
2022-04-10 16:01:00 +0800

    139.单词拆分.java

--- 139.单词拆分.java
+++ 139.单词拆分.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /*
  * @lc app=leetcode.cn id=139 lang=java
@@ -11,12 +10,11 @@ import java.util.Set;
 class Solution {
     // DP
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordDictSet = new HashSet(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;   // dp[i] 表示 0 - (i-1) 的字符是 wordDict 中的单词。
         for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {   // j:单词的分割点。
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) { // substring 的范围 [j, i)
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) { // substring 的范围 [j, i)
                     dp[i] = true;
                     break;
                 }
