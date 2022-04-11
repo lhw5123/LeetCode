0203e2e5dc3cbd222eeb0b275c74ed8dc330e817
hevin <hev1n@qq.com>
2022-04-11 21:42:05 +0800

    55.跳跃游戏.java

--- 55.跳跃游戏.java
+++ 55.跳跃游戏.java
@@ -8,11 +8,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        int rightMost = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i <= rightMost) {
-                rightMost = Math.max(rightMost, i + nums[i]);
-                if (rightMost >= n - 1) {
+        int rightMax = 0;
+        for (int i = 0; i < n; i++) {
+            if (i <= rightMax) {
+                rightMax = Math.max(rightMax, nums[i] + i);
+                if (rightMax >= n - 1) {
                     return true;
                 }
             }
