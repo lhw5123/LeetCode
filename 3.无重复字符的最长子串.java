59b9c77690fff07ff0fa8ae2f346c00b6fa073d7
hevin <hev1n@qq.com>
2022-05-29 08:56:19 +0800

    3.无重复字符的最长子串.java

--- 3.无重复字符的最长子串.java
+++ 3.无重复字符的最长子串.java
@@ -5,32 +5,29 @@ import java.util.Map;
  * @lc app=leetcode.cn id=3 lang=java
  *
  * [3] 无重复字符的最长子串
+ * 
+ * 子串是连续的。
+ * 
+ * Tips：滑动窗口。
  */
 
 // @lc code=start
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.isEmpty()) {
-            return 0;
-        }
-
-        Map<Character, Integer> window = new HashMap<>();
-
-        int left = 0, right = 0;
         int res = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0;
 
         while (right < s.length()) {
             Character c = s.charAt(right);
-            right++;
-            window.put(c, window.getOrDefault(c, 0) + 1);
-
-            while (window.get(c) > 1) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            while (map.get(c) > 1) {
                 Character d = s.charAt(left);
+                map.put(d, map.get(d) - 1);
                 left++;
-                window.put(d, window.get(d) - 1);
             }
-
-            res = Math.max(right - left, res);
+            res = Math.max(res, right - left + 1);
+            right++;
         }
 
         return res;
