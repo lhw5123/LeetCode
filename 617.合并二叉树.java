0e9957b798372a4ff813c9d83e45e5619add4e48
hevin <hev1n@qq.com>
2022-07-23 09:34:38 +0800

    617: optimize space utlization

--- 617.合并二叉树.java
+++ 617.合并二叉树.java
@@ -1,3 +1,5 @@
+import datastruct.TreeNode;
+
 /*
  * @lc app=leetcode.cn id=617 lang=java
  *
@@ -22,20 +24,16 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) {
-            return null;
-        }
-        if (root1 == null && root2 != null) {
+        if (root1 == null) {
             return root2;
         }
-        if (root1 != null && root2 == null) {
+        if (root2 == null) {
             return root1;
         }
-
-        TreeNode newRoot = new TreeNode(root1.val + root2.val);
-        newRoot.left = mergeTrees(root1.left, root2.left);
-        newRoot.right = mergeTrees(root1.right, root2.right);
-        return newRoot;
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
     }
 }
 // @lc code=end
