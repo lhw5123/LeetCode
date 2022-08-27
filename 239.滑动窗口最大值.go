bb4868d97d76aba19b6d77acebc54fcb4b08414e
hevin <hev1n@qq.com>
2022-08-27 10:08:51 +0800

    239

---
+++ 239.滑动窗口最大值.go
@@ -0,0 +1,103 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=239 lang=golang
+ *
+ * [239] 滑动窗口最大值
+ *
+ * https://leetcode-cn.com/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (49.60%)
+ * Likes:    1122
+ * Dislikes: 0
+ * Total Accepted:    176.9K
+ * Total Submissions: 356.8K
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+ * 个数字。滑动窗口每次只向右移动一位。
+ *
+ * 返回滑动窗口中的最大值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1], k = 1
+ * 输出：[1]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [1,-1], k = 1
+ * 输出：[1,-1]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：nums = [9,11], k = 2
+ * 输出：[11]
+ *
+ *
+ * 示例 5：
+ *
+ *
+ * 输入：nums = [4,-2], k = 2
+ * 输出：[4]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * -10^4
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+func maxSlidingWindow(nums []int, k int) []int {
+	if len(nums) == 0 || len(nums) < k {
+		return make([]int, 0)
+	}
+
+	res := make([]int, 0, len(nums)-k+1)
+	window := make([]int, 0, k) // store the index of nums
+	for i, v := range nums {    // if the left-most index is out of window, remove it.
+		if i >= k && window[0] <= i-k {
+			window = window[1:len(window)]
+		}
+		for len(window) > 0 && nums[window[len(window)-1]] < v {
+			window = window[0 : len(window)-1]
+		}
+		window = append(window, i)
+		if i >= k-1 {
+			res = append(res, nums[window[0]])
+		}
+	}
+	return res
+}
+
+// @lc code=end
