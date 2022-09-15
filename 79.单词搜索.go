16cceb6f9661c01172fde4bd01eeafcd76a58898
hevin <hev1n@qq.com>
2022-09-15 21:41:36 +0800

    79.go

--- 79.单词搜索.go
+++ 79.单词搜索.go
@@ -10,56 +10,52 @@ func exist(board [][]byte, word string) bool {
 	if rows == 0 {
 		return false
 	}
-
 	cols := len(board[0])
 	visited := make([]bool, rows*cols)
-
 	for x := 0; x < cols; x++ {
 		for y := 0; y < rows; y++ {
-			if dfs(board, word, x, y, 0, rows, cols, &visited) {
+			if dfs(board, word, y, x, 0, &visited) {
 				return true
 			}
 		}
 	}
-
 	return false
 }
 
-func dfs(board [][]byte, word string, x, y, start, rows, cols int, visited *[]bool) bool {
-	if start == len(word)-1 {
-		return board[y][x] == word[start]
+func dfs(board [][]byte, word string, y, x, index int, visited *[]bool) bool {
+	if index == len(word)-1 {
+		return board[y][x] == word[index]
 	}
 
-	if board[y][x] == word[start] {
-		index := getIndex(x, y, cols)
-		(*visited)[index] = true
-		for _, d := range directions {
-			newX := x + d[0]
-			newY := y + d[1]
-			newIndex := getIndex(newX, newY, cols)
-			if inArea(newX, newY, rows, cols) && !(*visited)[newIndex] {
-				if dfs(board, word, newX, newY, start+1, rows, cols, visited) {
+	if board[y][x] == word[index] {
+		rows := len(board)
+		cols := len(board[0])
+		(*visited)[getIndex(y, x, rows)] = true
+		for _, d := range dir {
+			ny := y + d[0]
+			nx := x + d[1]
+			if inArea(ny, nx, rows, cols) && !(*visited)[getIndex(ny, nx, rows)] {
+				if dfs(board, word, ny, nx, index+1, visited) {
 					return true
 				}
 			}
 		}
-		(*visited)[index] = false
+		(*visited)[getIndex(y, x, rows)] = false
 	}
-
 	return false
 }
 
-var directions = [][]int{
-	{-1, 0},
-	{0, -1},
-	{1, 0},
-	{0, 1},
+var dir = [][]int{
+	[]int{0, 1}, // [y, x]
+	[]int{0, -1},
+	[]int{1, 0},
+	[]int{-1, 0},
 }
 
-func inArea(x, y, rows, cols int) bool {
-	return x >= 0 && x < cols && y >= 0 && y < rows
+func inArea(y, x, rows, cols int) bool {
+	return y >= 0 && y < rows && x >= 0 && x < cols
 }
 
-func getIndex(x, y, cols int) int {
-	return y*cols + x
+func getIndex(y, x, rows int) int {
+	return x*rows + y
 }
