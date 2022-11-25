9887b9d5a89d0cf109abc8f5e8babc257cd2a772
hevin <hev1n@qq.com>
2022-11-26 07:56:00 +0800

    16.go

---
+++ 16.最接近的三数之和.go
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=golang
+ *
+ * [16] 最接近的三数之和
+ *
+ * https://leetcode.cn/problems/3sum-closest/description/
+ *
+ * algorithms
+ * Medium (45.38%)
+ * Likes:    1296
+ * Dislikes: 0
+ * Total Accepted:    424.4K
+ * Total Submissions: 936.7K
+ * Testcase Example:  '[-1,2,1,-4]\n1'
+ *
+ * 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+ *
+ * 返回这三个数的和。
+ *
+ * 假定每组输入只存在恰好一个解。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [-1,2,1,-4], target = 1
+ * 输出：2
+ * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [0,0,0], target = 1
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 3 <= nums.length <= 1000
+ * -1000 <= nums[i] <= 1000
+ * -10^4 <= target <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+import (
+	"math"
+	"sort"
+)
+
+func threeSumClosest(nums []int, target int) int {
+	n, res, diff := len(nums), 0, math.MaxInt32
+
+	if n > 2 {
+		sort.Ints(nums)
+		for i := 0; i < n-2; i++ {
+			if i > 0 && nums[i] == nums[i-1] {
+				continue
+			}
+			for j, k := i+1, n-1; j < k; {
+				sum := nums[i] + nums[j] + nums[k]
+				if abs(sum-target) < diff {
+					res, diff = sum, abs(sum-target)
+				}
+				if sum == target {
+					return res
+				} else if sum > target {
+					k--
+				} else {
+					j++
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+func abs(t int) int {
+	if t < 0 {
+		return -t
+	}
+	return t
+}
+
+// @lc code=end
+
