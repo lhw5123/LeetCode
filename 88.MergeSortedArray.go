f4dbaea084e605a187d765e9988c9f5ef5b35796
hevin <iserlohn44@gmail.com>
2025-01-12 16:25:33 +0800

    Add comment

--- 88.MergeSortedArray.go
+++ 88.MergeSortedArray.go
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+// 从后往前
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	for n != 0 {
 		if m != 0 && nums1[m-1] > nums2[n-1] {
