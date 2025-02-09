3cc4b49460b73f4dde6c22f411913740a8dc0c01
hevin <iserlohn44@gmail.com>
2025-02-09 13:58:13 +0800

    25.递归改迭代

--- 25.k-个一组翻转链表.go
+++ 25.k-个一组翻转链表.go
@@ -12,33 +12,71 @@
  *     Next *ListNode
  * }
  */
-// 翻转 k 个结点，那被翻转的最后一个结点的索引就是 k - 1。
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	node := head
-	for i := 0; i < k; i++ {
-		if node == nil {
-			return head
-		}
-		node = node.Next
-	}
-	newHead := reverse(head, node)
-	head.Next = reverseKGroup(node, k)
-	return newHead
-}
+	dummy := &ListNode{Next: head}
+	prev := dummy
 
-// 翻转从 first 到 last 部分的链表，如果 for 循环的终止条件是 cur != last，
-// 那 last 是不会被翻转的。
-func reverse(first *ListNode, last *ListNode) *ListNode {
-	var prev *ListNode
-	cur := first
-	for cur != last {
-		next := cur.Next
-		cur.Next = prev
-		prev = cur
-		cur = next
+	for {
+		node := prev.Next
+		for i := 0; i < k; i++ {
+			if node == nil {
+				return dummy.Next
+			}
+			node = node.Next
+		}
+
+		curr := prev.Next
+		next := curr.Next
+		for i := 0; i < k-1; i++ {
+			temp := next.Next
+			next.Next = curr
+			curr = next
+			next = temp
+		}
+
+		// set `prev` to next section starter
+		temp := prev.Next
+		prev.Next.Next = next
+		prev.Next = curr
+		prev = temp
 	}
-	return prev
 }
 
 // @lc code=end
+
+// 如果不足 k 个的部分也要反转呢？
+func reverseKGroup2(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{Next: head}
+	prev := dummy
+
+	for {
+		node := prev.Next
+		reverseCount := k - 1
+		for i := 0; i < k; i++ {
+			if node == nil {
+				reverseCount = i
+				break
+			}
+			node = node.Next
+		}
+
+		curr := prev.Next
+		next := curr.Next
+		for i := 0; i < reverseCount; i++ {
+			temp := next.Next
+			next.Next = curr
+			curr = next
+			next = temp
+		}
+
+		// set `prev` to next section starter
+		temp := prev.Next
+		prev.Next.Next = next
+		prev.Next = curr
+		prev = temp
+		if prev == nil {
+			return dummy.Next
+		}
+	}
+}
 
