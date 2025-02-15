317380e13f405e45b1760d39ddb6c32c401778bb
hevin <iserlohn44@gmail.com>
2025-02-15 13:48:50 +0800

    15.三数之和.go

--- 15.三数之和.go
+++ 15.三数之和.go
@@ -10,36 +10,45 @@ import "sort"
 // 思路：双指针。排序之后遍历 nums，对于其中的每个元素，再用类似两数之和的思路去找有没有和是 -index 的。
 func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
-	result, start, end, index, addNum, length := make([][]int, 0), 0, 0, 0, 0, len(nums)
-	for index = 1; index < length-1; index++ {
-		start, end = 0, length-1
-		if index > 1 && nums[index] == nums[index-1] {
-			start = index - 1
+
+	var (
+		res = make([][]int, 0)
+		n   = len(nums)
+	)
+
+	for i := 0; i < n-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		if nums[i]+nums[i+1]+nums[i+2] > 0 {
+			break
+		}
+		if nums[i]+nums[n-2]+nums[n-1] < 0 {
+			continue
 		}
 
-		for start < index && end > index {
-			if start > 0 && nums[start] == nums[start-1] {
-				start++
-				continue
-			}
-			if end < length-1 && nums[end] == nums[end+1] {
-				end--
-				continue
-			}
-			addNum = nums[start] + nums[end] + nums[index]
-			if addNum == 0 {
-				result = append(result, []int{nums[start], nums[end], nums[index]})
-				start++
-				end--
-			} else if addNum > 0 {
-				end--
+		left, right := i+1, n-1
+		for left < right {
+			s := nums[i] + nums[left] + nums[right]
+			if s > 0 {
+				right--
+			} else if s < 0 {
+				left++
 			} else {
-				start++
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left++
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				right--
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
 			}
 		}
 	}
 
-	return result
+	return res
 }
 
 // @lc code=end
